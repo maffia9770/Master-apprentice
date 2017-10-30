d692fb6b5344cc3e713ba7245bf4708cb3909fd8
Jonas <Jonas@JONAS-PC>
2017-10-30 22:00:24 +0100

    alert

--- Master/Master/Scripts/scripts.js
+++ Master/Master/Scripts/scripts.js
@@ -1,6 +1,5 @@
 ﻿function QuestAjax(Quest) {
     Quest = $('#Q'+Quest).text();
-    alert(Quest);
     $.ajax({
         type: 'POST',
         url: 'Home.aspx/DisplayQuest',
@@ -9,29 +8,11 @@
         dataType: 'json',
         success: function (data) {
             result = JSON.parse(data.d);
-            //alert(result);
             $("#quest").text(result[0].QuestName);
             $("#desc").text(result[0].QuestDes);
             $("#obj").text(result[0].QuestObj);
             $("#rew").text(result[0].QuestRew);
             $(".details").css("visibility", "visible");
-
-
-
-            /*//var result = JSON.stringify(data)
-            var result = data;
-            console.log();
-            alert(result);
-           // result = result.replace('[', '');
-            //result = result.replace(']', '');
-            alert(result);
-            //result = JSON.parse(result);
-            alert(result.QuestReward);
-            alert(result.d[0].QuestReward)
-            console.log(result.d);
-           // alert("ajaxsuccess");
-            alert(result.QuestName);
-            $("#quest").text(result.QuestName);*/
         },
         error: function () {
             alert("ajaxerror");
